dcd7e59fa23cf0c6d1852597be4b2cab482ca885
unknown <Bhavesh>
2021-04-12 18:13:10 +0530

    Camera Ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,86 @@
-import React, { Component } from 'react';
-import T2S from './components/TextToSpeech/T2S';
+import React, { Fragment, useState, useEffect } from "react";
+import { Camera } from "./components/camera/index";
+import { Root, Preview, Footer, GlobalStyle } from "./styles";
+import { createWorker } from 'tesseract.js';
+import Speech from 'react-speech';
+import './App.css';
 
-class App extends Component {
-  render(){
-    return (
-          <T2S/>
-    );
-  }
+function App() {
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const [cardImage, setCardImage] = useState();
+  const [text, setText] = useState(null);
+  const [load, setLoad] = useState(false);
+
+  useEffect(()=>{
+
+    async function opticalCharacterRecognition() {
+      const worker = createWorker({
+        logger: m => console.log(m),
+      });
+      try {
+        await worker.load();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        const { data: { text } } = await worker.recognize(URL.createObjectURL(cardImage));
+        setText(text);
+        console.log(text);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if(!text && cardImage) {
+      setLoad(true);
+      opticalCharacterRecognition();
+    }
+
+  }, [cardImage]);
+
+  useEffect(()=>{
+    if(load) setLoad(false);
+  },[text]);
+
+  return (
+    <Fragment>
+      <Root>
+        {isCameraOpen && (
+          <Camera
+            onCapture={blob => setCardImage(blob)}
+            onClear={() => {
+              setCardImage(undefined);
+              setText(null);
+              setLoad(false);
+            }}
+          />
+        )}
+
+        {cardImage && (
+          <div>
+            <h2>Preview</h2>
+            <Preview src={cardImage && URL.createObjectURL(cardImage)} />
+          </div>
+        )}
+
+        {
+          load 
+          ?
+          <div>Loading...</div>
+          :
+          <div>
+            <div>
+              {text}
+            </div>
+            <div>
+              {text && (<Speech style={{play:{button:{width:'100', height:'28'}}}} text={text}/>)}
+            </div>
+          </div>
+        }
+
+        <Footer>
+          <button onClick={() => setIsCameraOpen(true)}>Open Camera</button>
+        </Footer>
+      </Root>
+      <GlobalStyle />
+    </Fragment>
+  );
 }
-
 export default App;
